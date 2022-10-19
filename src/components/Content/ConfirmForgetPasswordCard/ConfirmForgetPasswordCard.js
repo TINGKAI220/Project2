5e22c60840f4db052e9d6eef487c7c04c0a4524e
BanqiaoCTK <tingkaichang1992@gmail.com>
2022-10-20 01:01:57 +0800

    first version

---
+++ src/components/Content/ConfirmForgetPasswordCard/ConfirmForgetPasswordCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ConfirmForgetPasswordCard.css";
+
+const ConfirmForgetPasswordCard = (props) => {
+  return (
+    <div className="ConfirmForgetPasswordCard">
+      We have sent the update password link to your email, please check that!
+    </div>
+  );
+};
+
+export default ConfirmForgetPasswordCard;
